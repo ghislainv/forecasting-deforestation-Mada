b5b36dc222a99cfb3e9859a4574d3cc9c4888672
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-08-31 17:38:42 +0200

    Update

--- R/dfp_plot.R
+++ R/dfp_plot.R
@@ -1,8 +1,8 @@
 require(broom)
 require(glue)
 require(rasterVis)
-require(scales)
-require(grid)
+require(scales) # for scales::squish()
+require(grid) # for grid::unit()
 
 # Theme
 ## Setting basic theme options for plot with ggplot2
@@ -13,19 +13,13 @@ theme_base <- theme(axis.line=element_blank(),
 										axis.title.x=element_blank(),
 										axis.title.y=element_blank(),
 										legend.position="none",
-										plot.margin=unit(c(0,0,0,0),"null"),
-										panel.spacing=unit(c(0,0,0,0),"null"),
+										plot.margin=grid::unit(c(0,0,0,0),"null"),
+										panel.spacing=grid::unit(c(0,0,0,0),"null"),
 										plot.background=element_rect(fill="transparent"),
 										panel.background=element_rect(fill="transparent"),
 										panel.grid.major=element_blank(),
 										panel.grid.minor=element_blank(),
 										panel.border=element_blank())
-
-# Zooms
-zoom1 <- list(xmin=346000,xmax=439000,ymin=7387000,ymax=7480000)
-zoom2 <- list(xmin=793000,xmax=886000,ymin=7815000,ymax=7908000)
-Extent.zoom1 <- paste(zoom1$xmin,zoom1$ymin,zoom1$xmax,zoom1$ymax)
-Extent.zoom2 <- paste(zoom2$xmin,zoom2$ymin,zoom2$xmax,zoom2$ymax)
 
 # rho_plot()
 rho_plot <- function(input_raster, input_vector, output_file, quantiles_legend=c(0.025,0.975), ...) {
@@ -34,15 +28,47 @@ rho_plot <- function(input_raster, input_vector, output_file, quantiles_legend=c
 	rho_bound <- max(sqrt(rho_quantiles^2))
 	rho_limits <- c(-rho_bound,rho_bound)
 	# Call to ggplot
-	rasterVis::gplot(input_raster) +
+	p <- rasterVis::gplot(input_raster) +
 		geom_raster(aes(fill=value)) +
 		scale_fill_gradientn(colours=c("forestgreen","yellow","red"),na.value="transparent",
 												 limits=rho_limits, oob=scales::squish) +
 		geom_polygon(data=broom::tidy(input_vector), aes(x=long, y=lat, group=id), colour="black", fill="transparent", size=0.3) +
-		theme_bw() + theme_base + coord_equal()
-		#coord_equal(xlim=c(300000,1100000),ylim=c(7165000,8685000))
+		theme_bw() + theme_base + coord_fixed()
 	# Save plot
 	ggsave(output_file, ...)
-	# Return message
-	return(glue("Results plotted to \"{output_file}\""))
+	# Return message an plot
+	cat(glue("Results plotted to \"{output_file}\""))
+	return(p)
 }
+
+# diff_plot()
+diff_plot <- function(input_raster, input_vector, output_file, maxpixels=50000, ext=NULL, rect=NULL, ...) {
+	# Crop raster
+	if (!is.null(ext)) {
+		input_raster <- crop(input_raster,ext)
+		e <- extent(ext)
+		xlim = c(xmin(e),xmax(e))
+		ylim = c(ymin(e),ymax(e))
+	} else {
+		xlim = NULL
+		ylim = NULL
+	}
+	# Call to ggplot
+	p <- rasterVis::gplot(input_raster, maxpixels=maxpixels) +
+		geom_raster(aes(fill=factor(value))) +
+		scale_fill_manual(values=c("red","forestgreen","darkblue","lightblue"), na.value="transparent") +
+		{if (!is.null(rect))
+			geom_rect(data=rect, inherit.aes=FALSE, aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,group=id),
+								fill="transparent", colour="black", size=0.3)
+		} +
+		geom_polygon(data=broom::tidy(input_vector), inherit.aes=FALSE, aes(x=long, y=lat, group=id),
+								 colour="black", fill="transparent", size=0.3) +
+		theme_bw() + theme_base + coord_fixed(ratio=1, xlim, ylim, expand=FALSE)
+	# Save plot
+	ggsave(output_file, p, ...)
+	# Return message and plot
+	cat(glue("Results plotted to \"{output_file}\""))
+	return(p)
+}
+
+
